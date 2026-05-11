4a13b31c9210f339b3c65645c05ac4b3493ffe87
Jacob <jacob@example.com>
2026-05-11 10:05:00 +0200

    Configuracion para despliegue automatico en la web (Netlify/Vercel)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [basicSsl()],
+  plugins: [],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
+  // Base path relative for more flexibility
+  base: '/',
 });
